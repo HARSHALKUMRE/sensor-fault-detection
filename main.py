c3c3876b6c96fae7511504fd7d9539a057c38669
Harshal Kumre <kumreharshalkumar@gmail.com>
2023-01-11 18:21:03 +0530

    data ingestion done

--- main.py
+++ main.py
@@ -2,20 +2,13 @@ from sensor.configuration.mongo_db_connection import MongoDBClient
 from sensor.exception import SensorException
 from sensor.logger import logging
 import os, sys
-
-def test_exception():
-    try:
-        logging.info("We are dividing 1 by zero")
-        x=1/0
-    except Exception as e:
-        #raise SensorException(e, sys)
-        raise e
+#from sensor.entity.config_entity import TrainingPipelineConfig, DataIngestionConfig
+from sensor.pipeline.training_pipeline import TrainPipeline
 
 
 if __name__ == '__main__':
-    try:
-        test_exception()
-    except Exception as e:
-        print(e)
-    #mongodb_client = MongoDBClient()
-    #print("Collection name: ",mongodb_client.database.list_collection_names())
+    #training_pipeline_config = TrainingPipelineConfig()
+    #data_ingestion_config = DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+    #print(data_ingestion_config.__dict__)
+    training_pipeline = TrainPipeline()
+    training_pipeline.run_pipeline()
--- sensor/configuration/mongo_db_connection.py
+++ sensor/configuration/mongo_db_connection.py
@@ -1,5 +1,7 @@
 import pymongo
+import os
 from sensor.constant.database import DATABASE_NAME
+from sensor.constant.env_variable import MONGODB_URL_KEY
 import certifi
 ca = certifi.where()
 
@@ -8,8 +10,12 @@ class MongoDBClient:
     def __init__(self, database_name=DATABASE_NAME) -> None:
         try:
             if MongoDBClient.client is None:
-                mongo_db_url = "mongodb+srv://harshal:Harsh123@cluster0.ppzqzhg.mongodb.net/?retryWrites=true&w=majority"
-                MongoDBClient.client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
+                mongo_db_url = "mongodb+srv://iNeuron:RsPGmJUa69n558n@ineuron-ai-projects.7eh1w4s.mongodb.net/?retryWrites=true&w=majority"#os.getenv(MONGODB_URL_KEY)
+                print(mongo_db_url)
+                if "localhost" in mongo_db_url:
+                    MongoDBClient.client = pymongo.MongoClient(mongo_db_url)
+                else:
+                    MongoDBClient.client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
             self.client = MongoDBClient.client
             self.database = self.client[database_name]
             self.database_name = database_name
