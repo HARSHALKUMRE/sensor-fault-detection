2f3ada73702ec541b6547dfd79bbedca12987251
Harshal Kumre <kumreharshalkumar@gmail.com>
2023-01-14 23:12:52 +0530

    updated

--- sensor/utils/main_utils.py
+++ sensor/utils/main_utils.py
@@ -4,7 +4,7 @@ import os,sys
 
 def read_yaml_file(file_path: str) -> dict:
     try:
-        with open(file_path, 'rb') as yaml_file:
+        with open(file_path, "rb") as yaml_file:
             return yaml.safe_load(yaml_file)
     except Exception as e:
         raise SensorException(e,sys) from e
